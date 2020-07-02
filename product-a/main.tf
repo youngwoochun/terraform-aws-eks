a23b9ae5fda4f80914e22fcd4d7da6052d66a52f
Young Chun <youngchun@Youngs-MacBook-Pro.local>
2020-07-02 10:40:22 -0400

    testing

--- product-a/main.tf
+++ product-a/main.tf
@@ -28,13 +28,13 @@ provider "kubernetes" {
 }
 
 module "eks" {
-  source = "../eks/"
+  source                      = "../eks/"
 
   cluster_name                         = var.cluster_name
   cluster_endpoint_public_access       = var.cluster_endpoint_public_access
   cluster_endpoint_private_access      = var.cluster_endpoint_private_access
   cluster_endpoint_public_access_cidrs = var.cluster_endpoint_public_access_cidrs
-  vpc_id                               = data.terraform_remote_state.network.outputs.vpc_id
+  vpc_id                               = data.terraform_remote_state.network.output.vpc_id
   cluster_security_group_id            = module.eks.cluster_sg_id
 
   asg_name                             = var.worker_group_defaults["asg_name"]
@@ -44,7 +44,7 @@ module "eks" {
   health_check_type                    = var.worker_group_defaults["health_check_type"]
   asg_desired_capacity                 = var.worker_group_defaults["asg_desired_capacity"]
   force_delete                         = var.worker_group_defaults["force_delete"]
-  private_subnet_id                    =
+  private_subnet_id                    = ["subnet-04e45f0cb72f9feb7", "subnet-062cf50d11fd5a7f5"]
 
   image_id                             = var.worker_group_defaults["image_id"]
   instance_type                        = var.worker_group_defaults["instance_type"]
